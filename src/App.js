a9e7acedd84fbc98bf7c09617314af6c3d766ecc
akashahamad <engr.akashahmad@gmail.com>
2020-09-11 17:17:23 +0500

    privacy-policy page added through routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import Hajira from './hajira/hajira'
+import Home from './hajira/hajira'
 import Update from './components/Update/update'
 import Add from './components/Add/add'
 import Sec1 from './components/sectionOne/sectionOne'
+import Privacypolicy from './components/privacypolicy/privacypolicy'
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 
 function App() {
@@ -10,10 +11,11 @@ function App() {
 
       <BrowserRouter>
         <Switch>
-          <Route path={"/"} exact={true} component={Hajira}/>
+          <Route path={"/"} exact={true} component={Home}/>
           <Route path={"/update"} component={Update}/>
             <Route path={"/add"} component={Add}/>
             <Route path={"/sec1"}  component={Sec1}/>
+            <Route path={"/privacy-policy"}  component={Privacypolicy}/>
         </Switch>
       </BrowserRouter>
 
